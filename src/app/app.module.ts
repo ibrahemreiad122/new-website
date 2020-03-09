32f541384e59d45dea50a84b5ffebaa7667524c1
ibrahem <ibrahem.reiad@gmail.com>
2020-03-09 02:22:12 +0200

    add tours section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { FeaturesComponent } from './features/features.component';
+import { ToursComponent } from './tours/tours.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    ToursComponent
   ],
   imports: [
     BrowserModule,
